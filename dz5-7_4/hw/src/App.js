e6613e7f78b064c073eddaa60d16d7fbd11835c5
Manty-S-Djusaem <104922578+Manty-S-Djusaem@users.noreply.github.com>
2023-04-08 16:06:36 +0600

    'fr'

---
+++ dz5-7_4/hw/src/App.js
@@ -0,0 +1,19 @@
+import { Route, Router, Routes } from "react-router-dom";
+import "./App.css";
+import AboutPokemon from "./components/AboutPokemon/AboutPokemon";
+import Menu from "./components/Menu/Menu";
+import MainPage from "./page/MainPage/MainPage";
+
+function App() {
+  return (
+    <>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<AboutPokemon />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
